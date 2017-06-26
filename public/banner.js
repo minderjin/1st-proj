d9f380a5843419f1e92da6c3414ef806749e1e59
minerjin <jiniusk@gmail.com>
2017-06-26 17:07:09 +0900

    - 배너 스크립트 추가

---
+++ public/banner.js
@@ -0,0 +1,14 @@
+$.ajax({
+    type: 'GET',
+    url: 'http://210.114.91.91:25182/banner',
+    data: { mid: 'tester1'
+    },
+    dataType: 'jsonp',
+    jsonp: 'callback',
+    success: function(response,opts) {
+        $('.adsbyckj').html(response.banner);    // banner 리푸레쉬
+    },
+    error: function(response,opts) {
+        console.log('error: ' + response + ', ' + opts);
+    }
+});
